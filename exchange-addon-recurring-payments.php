61b7066c06c8c3fda49537fc90771295da44c370
Lew Ayotte <lew@lewayotte.com>
2013-08-23 12:52:31 -0400

    first commit, recurring payments addon, moved from core

---
+++ exchange-addon-recurring-payments.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Plugin Name: iThemes Exchange - Recurring Payments Add-on
+ * Version: 1.0.0
+ * Description: Adds the recurring payments abilities to iThemes Exchange
+ * Plugin URI: http://ithemes.com/exchange/recurring-payments/
+ * Author: iThemes
+ * Author URI: http://ithemes.com
+ * iThemes Package: exchange-addon-recurring-payments
+ 
+ * Installation:
+ * 1. Download and unzip the latest release zip file.
+ * 2. If you use the WordPress plugin uploader to install this plugin skip to step 4.
+ * 3. Upload the entire plugin directory to your `/wp-content/plugins/` directory.
+ * 4. Activate the plugin through the 'Plugins' menu in WordPress Administration.
+ *
+*/
+
+define( 'ITE_RECURRING_PAYMENTS_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+/**
+ * This registers our plugin as a recurring payments addon
+ *
+ * @since 0.1.0
+ *
+ * @return void
+*/
+function it_exchange_register_recurring_payments_addon() {
+	$options = array(
+		'name'              => __( 'Recurring Payments', 'LION' ),
+		'description'       => __( 'This add-on turns on recurring payments for supporting payment gateways.', 'LION' ),
+		'author'            => 'iThemes',
+		'author_url'        => 'http://ithemes.com/exchange/recurring-payments/',
+	'icon'              => ITUtility::get_url_from_file( dirname( __FILE__ ) . '/images/recurringpayments50px.png' ),
+		'file'              => dirname( __FILE__ ) . '/init.php',
+		'category'          => 'other',
+	);
+	it_exchange_register_addon( 'recurring-payments', $options );
+}
+add_action( 'it_exchange_register_addons', 'it_exchange_register_recurring_payments_addon' );
+
+/**
+ * Loads the translation data for WordPress
+ *
+ * @uses load_plugin_textdomain()
+ * @since 1.0.0
+ * @return void
+*/
+function it_exchange_recurring_payments_set_textdomain() {
+	load_plugin_textdomain( 'LION', false, plugin_basename( __FILE__  ) . '/lang/' );
+}
+add_action( 'plugins_loaded', 'it_exchange_recurring_payments_set_textdomain' );
+
+/**
+ * Registers Plugin with iThemes updater class
+ *
+ * @since 1.0.0
+ *
+ * @param object $updater ithemes updater object
+ * @return void
+*/
+/*
+function ithemes_exchange_addon_stripe_updater_register( $updater ) { 
+	    $updater->register( 'exchange-addon-membership', __FILE__ );
+}
+add_action( 'ithemes_updater_register', 'ithemes_exchange_addon_membership_updater_register' );
+require( dirname( __FILE__ ) . '/lib/updater/load.php' );
+*/
---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+*/
+include( 'lib/required-hooks.php' );
+
+/**
+ * New Product Features added by the Exchange Membership Add-on.
+*/
+require( 'lib/product-features/load.php' );
