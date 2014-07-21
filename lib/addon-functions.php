f750e0aa30a15b3ff60f772c60013566f21f862e
Lew Ayotte <lew@ithemes.com>
2014-07-21 10:47:24 -0500

    User version_compare when comparing versions!

--- lib/addon-functions.php
+++ lib/addon-functions.php
@@ -47,7 +47,7 @@ function it_exchange_recurring_payments_customer_notification( $customer, $statu
  * @return void
 */
 function it_exchange_addon_recurring_payments_show_version_nag() {
-	if ( $GLOBALS['it_exchange']['version'] < '1.3.0' ) {
+	if ( version_compare( $GLOBALS['it_exchange']['version'], '1.3.0', '<' ) ) {
 		?>
 		<div id="it-exchange-add-on-min-version-nag" class="it-exchange-nag">
 			<?php printf( __( 'The Recurring Payments add-on requires iThemes Exchange version 1.3.0 or greater. %sPlease upgrade Exchange%s.', 'LION' ), '<a href="' . admin_url( 'update-core.php' ) . '">', '</a>' ); ?>
