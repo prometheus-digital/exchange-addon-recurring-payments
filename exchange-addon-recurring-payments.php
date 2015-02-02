e16cbb93451b458009ce36477f841ef8e79c0183
Lew Ayotte <lew@ithemes.com>
2015-02-02 09:43:02 -0600

    Oops, I mean taggign 1.0.22

--- exchange-addon-recurring-payments.php
+++ exchange-addon-recurring-payments.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Recurring Payments Add-on
- * Version: 1.0.21
+ * Version: 1.0.22
  * Description: Adds the recurring payments abilities to iThemes Exchange
  * Plugin URI: http://ithemes.com/exchange/recurring-payments/
  * Author: iThemes
