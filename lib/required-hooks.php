91fd1f98213540cc9e493dfe2673d1943561df0a
Timothy Jacobs <timothy@ironbounddesigns.com>
2017-02-16 11:18:41 -0500

    Make sure recurring label is only included once on sale price

--- lib/required-hooks.php
+++ lib/required-hooks.php
@@ -1144,7 +1144,7 @@ function it_exchange_recurring_payments_api_theme_product_base_price( $base_pric
 	return it_exchange_recurring_payments_addon_recurring_label( $product_id, true, $base_price ) ?: $base_price;
 }
 
-add_filter( 'it_exchange_api_theme_product_base_price', 'it_exchange_recurring_payments_api_theme_product_base_price', 100, 2 );
+add_filter( 'it_exchange_api_theme_product_base_price_full', 'it_exchange_recurring_payments_api_theme_product_base_price', 100, 2 );
 add_filter( 'it_exchange_customer_pricing_product_price', 'it_exchange_recurring_payments_api_theme_product_base_price', 10, 2 );
 add_filter( 'it_exchange_admin_product_list_price_column', 'it_exchange_recurring_payments_api_theme_product_base_price', 10, 2 );
 
